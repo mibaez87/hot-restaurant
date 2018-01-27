e87844b20b8279b8c372d41181d0051e2198fe34
Michel B <michelb@JB-MBA.local>
2018-01-27 12:21:47 -0500

    it works

--- server.js
+++ server.js
@@ -8,59 +8,45 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var reservations = [
-  {
-    routeName: "jane",
-    name: "Jane Doe",
-    phone_number: 1234567890,
-    email: "jane@me.com",
-    unique_id: 777
-  }
-];
+var reservations = [];
+var waitlist = [];
 
-var waitlist = [
-  {
-    routeName: "becky",
-    name: "Becky Jones",
-    phone_number: 7895671234,
-    email: "beckywiththegoodhair@me.com",
-    unique_id: 090
-  }
-];
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/api/tables", function(req,res){
+app.get("/api/tables", function (req, res) {
   res.json(reservations);
 });
 
-app.get("/api/waitlist", function(req,res){
+app.get("/api/waitlist", function (req, res) {
   res.json(waitlist);
 });
 
-app.post("/api/new", function (req,res){
+app.post("/api/new", function (req, res) {
   var newreservation = req.body;
 
   newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+  console.log(reservations.length);
 
-  console.log(newreservation);
-
-  reservations.push(newreservation);
-
+  if (reservations.length > 2) {
+    waitlist.push(newreservation);
+    console.log("WAITLIST");
+  } else {
+    reservations.push(newreservation);
+    console.log("RESERVATIONS");
+  }
   res.json(newreservation);
 });
 
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
